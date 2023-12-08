7ddb50fe9584205b02f64540e787e9366adb09a3
matheus0pestana@gmail.com <matheus0pestana@gmail.com>
2023-12-08 01:50:06 -0300

    Pequenas atualizações no css.

--- pedidos.php
+++ pedidos.php
@@ -37,7 +37,6 @@ if ($statement) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/cardapio.css">
     <link rel="stylesheet" href="assets/css/pedidos.css">
     <link rel="stylesheet" href="assets/css/responsivo_pedidos.css">
     <link rel="shortcut icon" type="imagex/png" href="assets/img/LogoChickito.png">
